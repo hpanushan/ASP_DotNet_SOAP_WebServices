07f1b46950c20894ba59df6665f8a7a12fd6b86a
anushanram <hpanushan@gmail.com>
2019-05-27 18:14:42 +0530

    Created web service

---
+++ SOAP_WebServices/SOAP_WebServices/DataRetriver.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SOAP_WebServices
+{
+    /// <summary>
+    /// Summary description for DataRetriver
+    /// </summary>
+    [WebService(Namespace = "http://hpanushan.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class DataRetriver : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
